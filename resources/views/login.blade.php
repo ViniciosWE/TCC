e65ee2e35bfa3fae0401ad37bb475559c8de1c71
vinicios <vinicioswe2005@gmail.com>
2026-07-10 14:09:41 -0300

    arrumando tela login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,16 @@
-@extends('LayoutInicial')
+<!DOCTYPE html>
+<html lang="pt-br">
 
-@section('title', 'Login')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <title>Login</title>
+</head>
+    <h1>Página de login</h1>
+    <a href="{{ route('PaginaInicial') }}">Voltar</a>
+<body>
 
-@section('content')
-    <h1>Bem vindo à Página de Login</h1>
-@endsection
+</body>
+
+</html>
