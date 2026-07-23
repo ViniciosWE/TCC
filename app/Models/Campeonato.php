69e376e76441216352e2c9d2e7d64af0a1b2159e
vinicios <vinicioswe2005@gmail.com>
2026-07-23 10:11:36 -0300

    fazendo as models

--- app/Models/Campeonato.php
+++ app/Models/Campeonato.php
@@ -8,4 +8,43 @@ use Illuminate\Database\Eloquent\Model;
 class Campeonato extends Model
 {
     use HasFactory;
+
+     protected $fillable = [
+        'user_id',
+        'minimo_jogadores_equipes',
+        'maximo_equipes',
+        'nome',
+        'categoria',
+        'data_inicio',
+        'data_fim',
+        'status',
+    ];
+
+
+    /* Um campeonato pertence ao usuário que criou */
+    public function usuario()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    /* Um campeonato pode possuir várias notícias */
+    public function noticias()
+    {
+        return $this->hasMany(Noticia::class);
+    }
+
+
+    /* Um campeonato possui várias partidas */
+    public function partidas()
+    {
+        return $this->hasMany(Partida::class);
+    }
+
+
+    /* Um campeonato possui várias inscrições de equipes */
+    public function inscricoes()
+    {
+        return $this->hasMany(Inscricao::class);
+    }
 }
--- app/Models/Participante.php
+++ app/Models/Participante.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Participante extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'numero',
+        'funcao',
+        'nome',
+        'cpf',
+        'status',
+    ];
+
+
+    /* Um participante pode possuir vários contratos com equipes */
+    public function contratos()
+    {
+        return $this->hasMany(Contrato::class);
+    }
+
+
+    /* Um participante pode possuir vários eventos registrados em partidas */
+    public function eventosPartidas()
+    {
+        return $this->hasMany(EventoPartida::class);
+    }
 }
