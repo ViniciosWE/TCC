9290d4b669777f28248d02d827f5fb7211f9e406
vinicios <vinicioswe2005@gmail.com>
2026-07-31 14:15:21 -0300

    deixando sigla e nome de euquipes unicas

--- database/migrations/2026_07_22_130939_create_equipes_table.php
+++ database/migrations/2026_07_22_130939_create_equipes_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('equipes', function (Blueprint $table) {
             $table->id();
-            $table->string('nome');
+            $table->string('nome')->unique();
             $table->string('escudo');
-            $table->char('sigla',3);
+            $table->char('sigla',3)->unique();
             $table->enum('status',['ATIVA','ENCERRADA', 'SUSPENSA']);
             $table->timestamps();
         });
